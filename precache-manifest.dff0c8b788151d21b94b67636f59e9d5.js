001f125e5ad4a6de4e51cd652fe25aa2bb8931b9
Jacob Krol <phone12.jk@gmail.com>
2020-06-20 14:45:18 -0500

    Updates

---
+++ precache-manifest.dff0c8b788151d21b94b67636f59e9d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85231aa8cc2b8954fa1a26a4fc336376",
+    "url": "/LP-Timer/index.html"
+  },
+  {
+    "revision": "bf60077704032e5eebe3",
+    "url": "/LP-Timer/static/css/main.f5cfbc1f.chunk.css"
+  },
+  {
+    "revision": "14ff47a7c399ea186a76",
+    "url": "/LP-Timer/static/js/2.7457e1e0.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/LP-Timer/static/js/2.7457e1e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf60077704032e5eebe3",
+    "url": "/LP-Timer/static/js/main.88e283cb.chunk.js"
+  },
+  {
+    "revision": "29ebc8d7e5bd818a8e01",
+    "url": "/LP-Timer/static/js/runtime-main.4d0928de.js"
+  },
+  {
+    "revision": "387bfa743c08d9ca6fd40413652174f0",
+    "url": "/LP-Timer/static/media/30-seconds-verbal.387bfa74.wav"
+  },
+  {
+    "revision": "3ce36467febacea6f8afcce179f3ca5d",
+    "url": "/LP-Timer/static/media/60-seconds-verbal.3ce36467.wav"
+  },
+  {
+    "revision": "d15918e6e0f4eac20cdd3ca08e874b2d",
+    "url": "/LP-Timer/static/media/90-seconds-verbal.d15918e6.wav"
+  },
+  {
+    "revision": "ab0e848e0e83a85a5af69f5acf1ebe73",
+    "url": "/LP-Timer/static/media/cursed-timer.ab0e848e.ttf"
+  },
+  {
+    "revision": "08d37409a924ae95fff8df77747b0c42",
+    "url": "/LP-Timer/static/media/titillium-web-bold.08d37409.ttf"
+  },
+  {
+    "revision": "ce96d75e97d58b7396e1431557bb02a0",
+    "url": "/LP-Timer/static/media/titillium-web-semi-bold.ce96d75e.ttf"
+  }
+]);
