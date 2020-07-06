fa1cd78163d7cca02caec7b5f4a317edfadaeb6e
Jacob Krol <phone12.jk@gmail.com>
2020-07-06 18:37:11 -0500

    Fix headers appearing behind home button on small screens

--- src/components/utilities/Footer.js
+++ src/components/utilities/Footer.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <footer>
-            <p>LP Timer PWA v1.1.0 - Copyright 2020 <a href="https://kroljs.com" target="_blank" rel="noopener noreferrer">Jacob Krol</a> - <Link to="/about">Learn More</Link></p>
+            <p>LP Timer PWA v1.1.1 - Copyright 2020 <a href="https://kroljs.com" target="_blank" rel="noopener noreferrer">Jacob Krol</a> - <Link to="/about">Learn More</Link></p>
         </footer>
     )
 }
